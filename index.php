6dfa171e437ef69c44a9980cd8aefe60f9277924
regilanmeira <regilan@hotmail.com>
2024-06-12 15:25:37 -0300

    Defini o index e o entrarconta como responsivo bem como configurar o container+linha_col

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=" device-widt, initial-scale=1.0">
+    
+    <?php
+
+    require "referencias.php" ?>
+</head>
+
+<body>
+
+    <div class="container">
+        <form method="post">
+            <div id="bemvindo">
+                <div class="container">
+                    <img src="imagens/reciclagem_maos.png" alt="Kattar Logo" class="logo">
+                    <h1 style="color: #278C40;">Kattar</h1>
+                    <h1>Bem-vindo!</h1>
+                    <div class="button-container">
+                        <button class="btn btn-sucess" formaction="entrar_conta.php">Entrar</a>
+                        <button class="btn btn-sucess" formaction="criar_conta.php">Criar conta</a>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+
+
+</body>
+
+</html>
---
+++ referencias.php
@@ -0,0 +1,17 @@
+<!-- REFERÊNCIAS PARA LINKS EXTERNOS, CSS, ETC -->
+
+<title>Kattar - Solutech</title>
+
+
+
+
+<link href="css/bootstrap.min.css" rel="stylesheet" />
+<link href="css/estilo.css" rel="stylesheet" />
+<script src="js/bootstrap.bundle.min.js"></script>
+
+
+
+
+
+
+
