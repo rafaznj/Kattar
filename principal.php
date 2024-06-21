227ff0b7df0857419dddf066ca58fef5bd33f733
Rafael Sena <163608557+rafaznj@users.noreply.github.com>
2024-06-21 20:12:20 -0300

    ajustes

--- principal.php
+++ principal.php
@@ -5,29 +5,36 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <?php require "referencias.php"?>
+    <?php require "referencias.php" ?>
 
 
 </head>
 
-<body>
-    <form>
+<body onload="getLocation()">
+<form   name="localizacao" id="localizacao" method="post">
+        <!-- AQUI OS CAMPOS SERÃO USADOS PARA CAPTURAR A LATITUDE E LONGITUDE E GUARDAR EM INPUTS ESCONDIDAS -->
+        <input type="hidden" class="input" id="txtLatitude" name="txtLatitude">
+        <input type="hidden" class="input" id="txtLongitude" name="txtLongitude">
+        
     <div class="container">
         <div class="row">
             <?php require "cabecalho_pagina.php" ?>
 
             <div class="col-md-4">
-                <input type="submit" class="btn btn-success" formaction="coleta_solicitar.php" value="Solicitar coleta">
+                <input type="submit" class="btn btn-success" formaction="coleta_historico.php" value="Histórico de coletas" />
             </div>
+
             <div class="col-md-4">
-                <input type="submit" class="btn btn-success" formaction="coleta_historico.php" value="Histórico de coletas">
+                <input type="submit" class="btn btn-success" formaction="coleta_instrucoes.php" value="Instruções de descarte" />
             </div>
+
             <div class="col-md-4">
-                <input type="submit" class="btn btn-success" formaction="coleta_instrucoes.php" value="Informativos">
+                <input type="submit" class="btn btn-success" formaction="coleta_mostrar_locacalizacao.php"  value="Pontos de coleta" name="btLocalizacao" id="btLocalizacao"/>
             </div>
+
         </div>
 
-            <?php require "rodape.php" ?>
+        <?php require "rodape.php" ?>
 
     </div>
 
--- rodape.php
+++ rodape.php
@@ -6,7 +6,7 @@
         <table style="width: 100%;">
             <tr style="width: 100%;">
                 <td style="width: 25%;"> <a href="principal.php"><i class="fa-solid fa-house fa-2xl" style="color: #198754;"></i></a></td>
-                <td style="width: 25%;"><a href="locacalizacao_ecopontos.php"><i class="fa-regular fa-square-plus fa-2xl" style="color: #198754;"></i></td>
+                <td style="width: 25%;"><a href="coleta_solicitar.php"><i class="fa-regular fa-square-plus fa-2xl" style="color: #198754;"></i></td>
                 <td style="width: 25%;"><a href="configuracoes.php"><i class="fa-solid fa-gear fa-2xl" style="color: #198754;"></i></a></td>
             </tr>
         </table>
